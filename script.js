b29e50ba9c14ac0b39c382fe8fed869475f3abff
Nitish kumar yadav <n55719271@gmail.com>
2026-03-17 18:39:27 +0530

    learning java

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 console.log("hello world");
 console.log("this is my first java script code");
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c); 
+var num=10;
+console.log(num);
+console.log(typeof num);
+var name="john";
+console.log(name);
+console.log(typeof name);
+num=false;
+console.log(num);
+console.log(typeof num);
+num="nitish";
+console.log(num);
+console.log(typeof num);
+var num = 10;
+console.log(num);
+console.log(typeof num);
+
+var personName = "john";
+console.log(personName);
+console.log(typeof personName);
+
+num = false;
+console.log(num);
+console.log(typeof num);
+
+num = "nitish";
+console.log(num);
+console.log(typeof num);
