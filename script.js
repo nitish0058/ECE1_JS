a53437d8fc6bb2a2a299ec22a5a43422647fbfb6
Nitish kumar yadav <n55719271@gmail.com>
2026-03-17 18:44:39 +0530

    java conditional statements

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-console.log("hello world");
-console.log("this is my first java script code");
-var a = 10;
-var b = 20;
-var c = a + b;
-console.log(c); 
-var num=10;
-console.log(num);
-console.log(typeof num);
-var name="john";
-console.log(name);
-console.log(typeof name);
-num=false;
-console.log(num);
-console.log(typeof num);
-num="nitish";
-console.log(num);
-console.log(typeof num);
-var num = 10;
-console.log(num);
-console.log(typeof num);
+// console.log("hello world");
+// console.log("this is my first java script code");
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log(c); 
+// var num=10;
+// console.log(num);
+// console.log(typeof num);
+// var name="john";
+// console.log(name);
+// console.log(typeof name);
+// num=false;
+// console.log(num);
+// console.log(typeof num);
+// num="nitish";
+// console.log(num);
+// console.log(typeof num);
+// var num = 10;
+// console.log(num);
+// console.log(typeof num);
 
-var personName = "john";
-console.log(personName);
-console.log(typeof personName);
+// var personName = "john";
+// console.log(personName);
+// console.log(typeof personName);
 
-num = false;
-console.log(num);
-console.log(typeof num);
+// num = false;
+// console.log(num);
+// console.log(typeof num);
 
-num = "nitish";
-console.log(num);
-console.log(typeof num);
+// num = "nitish";
+// console.log(num);
+// console.log(typeof num);
+
+const sym1 = Symbol(4);
+const sym2 = Symbol(4);
+
+console.log(sym1);
+console.log(sym2);
+if(sym1 === sym2){
+    console.log("sym1 and sym2 are equal");
+} else {
+    console.log("sym1 and sym2 are not equal");
+}
